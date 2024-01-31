8a8b59b53f55476d82aaf9a79138e69167d65254
Dowon <ks2515@naver.com>
2024-01-31 14:22:14 +0900

    fzf: wheel tags test

--- fzf/pdm_build.py
+++ fzf/pdm_build.py
@@ -1,8 +1,16 @@
+from __future__ import annotations
+
 import os
 import shutil
 import subprocess
 import sys
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+from wheel.cli.tags import tags
+
+if TYPE_CHECKING:
+    from pdm.backend.hooks import Context
 
 NAME = "fzf"
 VERSION = "0.46.0"
@@ -36,13 +44,18 @@ def build(output: str) -> None:
     Path(output).chmod(0o777)
 
 
-def pdm_build_hook_enabled(context):
+def pdm_build_hook_enabled(context: Context):
     return context.target != "sdist"
 
 
-def pdm_build_initialize(context) -> None:
+def pdm_build_initialize(context: Context) -> None:
     context.ensure_build_dir()
     output_path = Path(context.build_dir, "bin", NAME)
     if is_windows():
         output_path = output_path.with_suffix(".exe")
     build(str(output_path))
+
+
+def pdm_build_finalize(context: Context, artifact: Path) -> None:
+    renamed = tags(str(artifact), python_tags="py3", abi_tags="none", remove=True)
+    print(renamed)
