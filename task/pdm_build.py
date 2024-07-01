9b1afbba535ca4b46de02ef84a6ad485f23f7240
Dowon <ks2515@naver.com>
2024-07-01 15:49:14 +0900

    Revert "go-task: manual version fix"
    This reverts commit 45c4dc05a9d092733c7ca190b8eca9a06b18f3d4.

--- task/pdm_build.py
+++ task/pdm_build.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 import os
-import re
 import shutil
 import subprocess
 import sys
-from contextlib import contextmanager
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -46,25 +44,6 @@ def build(output: str) -> None:
     Path(output).chmod(0o777)
 
 
-@contextmanager
-def fix_task_version():
-    target = Path(__file__).parent.joinpath(NAME, "internal/version/version.go")
-    content = target.read_text(encoding="utf-8")
-
-    new = re.sub(r"return version", f'return "{VERSION}"', content)
-    new = re.sub(
-        r'return fmt.Sprintf\("%s \(%s\)", version, sum\)',
-        f'return fmt.Sprintf("%s (%s)", "{VERSION}", "pypi")',
-        new,
-    )
-    target.write_text(new, encoding="utf-8")
-
-    try:
-        yield
-    finally:
-        target.write_text(content, encoding="utf-8")
-
-
 def pdm_build_hook_enabled(context):
     return context.target != "sdist"
 
@@ -74,8 +53,7 @@ def pdm_build_initialize(context) -> None:
     output_path = Path(context.build_dir, "bin", NAME)
     if is_windows():
         output_path = output_path.with_suffix(".exe")
-    with fix_task_version():
-        build(str(output_path))
+    build(str(output_path))
 
 
 def pdm_build_finalize(context: Context, artifact: Path) -> None:
