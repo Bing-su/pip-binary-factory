3966ce4700cd6191c3b6791edb7d10d7e4e5b842
Dowon <ks2515@naver.com>
2024-10-25 13:31:13 +0900

    act: add act 0.2.68

---
+++ act/build_wheel.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+linux_platform_map = {
+    "manylinux2014_x86_64": "manylinux_2_17_x86_64.manylinux2014_x86_64.musllinux_1_1_x86_64",
+    "manylinux2014_aarch64": "manylinux_2_17_aarch64.manylinux2014_aarch64.musllinux_1_1_aarch64",
+}
+
+
+def build(os_: str, arch: str, platform: str):
+    os.environ["GOOS"] = os_
+    os.environ["GOARCH"] = arch
+    os.environ["CGO_ENABLED"] = "0"
+
+    args = [
+        sys.executable,
+        "-m",
+        "build",
+        "-w",
+        "--installer",
+        "uv",
+        f"--config-setting=--plat-name={platform}",
+    ]
+
+    subprocess.run(args, check=True)  # noqa: S603
+
+    if "linux" not in platform:
+        return
+
+    platform_tag = linux_platform_map[platform]
+
+    whl = next(Path("dist").glob(f"*{platform}*"))
+
+    args = [
+        sys.executable,
+        "-m",
+        "wheel",
+        "tags",
+        "--remove",
+        "--platform-tag",
+        platform_tag,
+        str(whl),
+    ]
+    subprocess.run(args, check=True)  # noqa: S603
+
+
+def main():
+    matrix = [
+        ("windows", "amd64", "win_amd64"),
+        ("windows", "arm64", "win_arm64"),
+        ("darwin", "amd64", "macosx_10_7_x86_64"),
+        ("darwin", "arm64", "macosx_11_0_arm64"),
+        ("linux", "amd64", "manylinux2014_x86_64"),
+        ("linux", "arm64", "manylinux2014_aarch64"),
+    ]
+
+    for os_, arch, platform in matrix:
+        build(os_, arch, platform)
+
+
+if __name__ == "__main__":
+    main()
