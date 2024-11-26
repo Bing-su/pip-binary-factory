2f73f3e09a5948a6b7011c9f8750480375d9e943
Dowon <ks2515@naver.com>
2024-11-26 21:35:38 +0900

    pocketbase: update build wheel script, gh actions

--- pocketbase/build_wheel.py
+++ pocketbase/build_wheel.py
@@ -2,11 +2,9 @@ import os
 import subprocess
 import sys
 from pathlib import Path
-from platform import machine, system
 
 linux_platform_map = {
-    "manylinux_2_28_x86_64": "manylinux_2_28_x86_64",
-    "musllinux_1_1_x86_64": "manylinux_2_17_x86_64.manylinux2014_x86_64.musllinux_1_1_x86_64",
+    "manylinux2014_x86_64": "manylinux_2_17_x86_64.manylinux2014_x86_64.musllinux_1_1_x86_64",
     "manylinux2014_aarch64": "manylinux_2_17_aarch64.manylinux2014_aarch64.musllinux_1_1_aarch64",
     "manylinux2014_s390x": "manylinux_2_17_s390x.manylinux2014_s390x.musllinux_1_1_s390x",
     "manylinux2014_ppc64le": "manylinux_2_17_ppc64le.manylinux2014_ppc64le.musllinux_1_1_ppc64le",
@@ -16,19 +14,7 @@ linux_platform_map = {
 def build(os_: str, arch: str, platform: str):
     os.environ["GOOS"] = os_
     os.environ["GOARCH"] = arch
-
-    if (
-        os_ == "linux"
-        and arch == "amd64"
-        and "musl" not in platform
-        and system() == "Linux"
-        and machine() == "x86_64"
-    ):
-        os.environ["CGO_ENABLED"] = "1"
-        os.environ["CC"] = "zig cc -target x86_64-linux-gnu"
-        os.environ["CXX"] = "zig c++ -target x86_64-linux-gnu"
-    else:
-        os.environ["CGO_ENABLED"] = "0"
+    os.environ["CGO_ENABLED"] = "0"
 
     args = [
         sys.executable,
@@ -68,8 +54,7 @@ def main():
         ("windows", "arm64", "win_arm64"),
         ("darwin", "amd64", "macosx_10_7_x86_64"),
         ("darwin", "arm64", "macosx_11_0_arm64"),
-        ("linux", "amd64", "manylinux_2_28_x86_64"),
-        ("linux", "amd64", "musllinux_1_1_x86_64"),
+        ("linux", "amd64", "manylinux2014_x86_64"),
         ("linux", "arm64", "manylinux2014_aarch64"),
         ("linux", "s390x", "manylinux2014_s390x"),
         ("linux", "ppc64le", "manylinux2014_ppc64le"),
