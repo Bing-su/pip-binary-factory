205849c696d4298a6b07e1174fabe543abeaff56
Dowon <ks2515@naver.com>
2025-04-16 14:19:51 +0900

    pocketbase: update to 0.27.0

--- pocketbase/build_wheel.py
+++ pocketbase/build_wheel.py
@@ -1,3 +1,9 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "wheel",
+# ]
+# ///
 import os
 import subprocess
 import sys
@@ -17,12 +23,9 @@ def build(os_: str, arch: str, platform: str):
     os.environ["CGO_ENABLED"] = "0"
 
     args = [
-        sys.executable,
-        "-m",
-        "build",
-        "-w",
-        "--installer",
         "uv",
+        "build",
+        "--wheel",
         f"--config-setting=--plat-name={platform}",
     ]
 
